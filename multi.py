937e797a6161497e2fcae3b6106b5419b76ad794
SolitarySpiral <solitary.ratty@gmail.com>
2023-11-24 23:08:54 +0500

    multi

---
+++ multi.py
@@ -0,0 +1,193 @@
+def get_multi(from_r34: bool):
+    small_multilist = []
+    full_multilist = []
+    if from_r34: # RULE 34 list tags
+        #1
+        positive_tags = ['nopanani']
+        extra_tags = []
+        negative_tags = ['big_ass', 'animated']
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #2
+        positive_tags = ['jashinn']
+        extra_tags = []
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #3
+        positive_tags = ['kgovipositors']
+        extra_tags = []
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+    else:        # NOZOMI list tags
+        #1
+        positive_tags = ['Riley_Anderson']
+        extra_tags = ['Riley', 'rileyandersen', 'riley_andersen']
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #2
+        positive_tags = ['Vanellope']
+        extra_tags = ['vanellope', 'vanellope_von_schweetz']
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #3
+        positive_tags = ['Sarah']
+        extra_tags = ['sarah_(the_last_of_us)', 'sarah_miller']
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #4
+        positive_tags = ['higegepon']
+        extra_tags = ['artist:ひ~げぇぽん','pixiv_id_54698934']
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #5
+        positive_tags = ['marie_rose']
+        extra_tags = ['マリー・ローズ','marie', 'marierose']
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #6
+        positive_tags = ['opossumachine']
+        extra_tags = ['artist:PossumMachine⚠️']
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #7
+        positive_tags = ['artist:7738']
+        extra_tags = ['hebe', 'pixiv_id_66553761']
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #8
+        positive_tags = ['artist:ボッシー']
+        extra_tags = ['pixiv_id_13450661']
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #9
+        positive_tags = ['artist:omsgarou']
+        extra_tags = ['pixiv_id_2297160']
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #10
+        positive_tags = ['artist:ターちゃん']
+        extra_tags = ['pixiv_id_947930']
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #11
+        positive_tags = ['artist:Libidoll']
+        extra_tags = ['pixiv_id_1043474']
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #12
+        positive_tags = ['artist:AliceBunnie']
+        extra_tags = ['pixiv_id_25867890']
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #13
+        positive_tags = ['lesdias']
+        extra_tags = ['artist:SPICYdias', 'pixiv_id_15079627', 'artist:irispoplar', 'irispoplar', 'pixiv_id_25423811']
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #14
+        positive_tags = ['flatculture']
+        extra_tags = []
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #15
+        positive_tags = ['asakuraf']
+        extra_tags = []
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #15
+        positive_tags = ['crumbles']
+        extra_tags = []
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #16
+        positive_tags = ['artist:imbi']
+        extra_tags = []
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #17
+        positive_tags = ['artist:VinnyInnocent']
+        extra_tags = ['pixiv_id_31938779']
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #18
+        positive_tags = ['artist:Damz']
+        extra_tags = ['pixiv_id_45855571']
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #19
+        positive_tags = ['artist:seven']
+        extra_tags = ['pixiv_id_12509439']
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+        #20
+        positive_tags = ['artist:lunarctic']
+        extra_tags = ['pixiv_id_15473311']
+        negative_tags = []
+        small_multilist.extend((positive_tags, extra_tags, negative_tags))
+        full_multilist.append(small_multilist)
+        small_multilist = []
+    return full_multilist
+
+'''
+#1-----WITH DATE
+#relevant_date = datetime.strptime("2023-11-11", '%Y-%m-%d')
+positive_tags = ['sherry_birkin']
+extra_tags = ['Sherry_Birkin']
+negative_tags = ['resident_evil_6', 'kitsunerider', 'best_bes', 'pixiv_id_14250783', 'sawao']
+full_multilist.append(small_multilist)
+small_multilist = []
+#2
+relevant_date = datetime.strptime("2023-11-11", '%Y-%m-%d')
+positive_tags = ['artist:ポザ孕 / pozahara']
+extra_tags = ['pixiv_id_448840']
+negative_tags = []
+small_multilist.extend((positive_tags, extra_tags, negative_tags))
+full_multilist.append(small_multilist)
+small_multilist = []
+
+'''#----WITH DATE END
